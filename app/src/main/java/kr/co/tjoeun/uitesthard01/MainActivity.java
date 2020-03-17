17f70ae066320dfc44695565c75586973df70ba4
cyhaz <cyhaz@naver.com>
2020-03-17 12:48:54 +0900

    회원가입 버튼 클릭 시 안내 토스트 출력

--- app/src/main/java/kr/co/tjoeun/uitesthard01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/uitesthard01/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginBtn = null;
     TextView findPwTxt = null;
+    TextView signUpTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         loginBtn = findViewById(R.id.loginBtn);
         findPwTxt = findViewById(R.id.findPwTxt);
+        signUpTxt = findViewById(R.id.signUpTxt);
 
 //        로그인버튼의 글자를 => 회원가입으로 바꾸자
         loginBtn.setText("회원가입");
@@ -52,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//       회원가입을 누루면 => 회원가입 화면으로 이동합니다 하는 안내문구 출력
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "회원가입 화면으로 이동합니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
