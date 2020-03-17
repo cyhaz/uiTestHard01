4ff144eac3010ae9f470c07d5e36711339cc2b05
cyhaz <cyhaz@naver.com>
2020-03-17 10:01:55 +0900

    로그인 화면 수정 회원가입 화면 생성

---
+++ app/src/main/java/kr/co/tjoeun/uitesthard01/SignUpActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.uitesthard01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
